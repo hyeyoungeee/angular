1580751e30a64ccf4316c8cc7b3b687912075080
hyjung <hyoung3639@gmail.com>
2017-11-08 17:39:04 +0900

    8. component style

--- src/app/todo/todos/todo/todo.component.ts
+++ src/app/todo/todos/todo/todo.component.ts
@@ -7,7 +7,35 @@ import { Todo } from '../../share/todo.model';
   template: `
    <input type = "checkbox" [checked] = "todo.done">{{todo.text}}
   `,
-  styles: []
+  styles: [`
+    :host {
+      display : block;
+      padding : 16px;
+      color : red;
+      background-color : white;
+    }
+
+    input {
+      position : releative;
+    }
+
+    input:before {
+      content : "";
+      display:inline-block;
+      width : 20px;
+      height : 20px;
+      background-color : white;
+      position : absolute;
+      top : -6px;
+      left : -6px;
+      border : 1px solid darkgray;
+    }
+
+    input:checked:after{
+      content : '\\2713';
+
+    }
+  `]
 })
 
 export class TodoComponent implements OnInit {
